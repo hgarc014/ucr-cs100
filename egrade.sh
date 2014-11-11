21b9b8efdcedcc168e1956679135c2bc091011cf
Henry Garcia <hgarc014@ucr.edu>
2014-11-10 23:02:58 -0800

    added back grade script

---
+++ egrade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=all.rem
+I=1
+> $FILE
+
+for INST in people/students/*;do
+        STR=${INST##*/}
+        echo $I: $STR | tee -a $FILE
+        let I=I+1
+        scripts/calcgrade.sh $STR | tail -4 >> $FILE
+    done
+
